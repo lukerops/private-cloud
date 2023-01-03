f1e25845e27a81100a71fa173dc32bac5bf195ac
Lucas Campos Vieira <lucasscvvieira@gmail.com>
2023-01-03 20:01:23 -0300

    ajusta a escolha da versão do kube-vip

--- modules/kubevip/locals.tf
+++ modules/kubevip/locals.tf
@@ -6,7 +6,7 @@ locals {
   versions = [
     for obj in jsondecode(data.http.kube_vip_versions.response_body) : obj.name
   ]
-  version = var.versioning.version == null ? local.versions[0] : setintersection([var.versioning.version], local.versions)[0]
+  version = var.versioning.version == null ? local.versions[0] : tolist(setintersection([var.versioning.version], local.versions))[0]
 
   envs = {
     vip_arp            = true
@@ -16,7 +16,6 @@ locals {
     cp_enable          = true
     cp_namespace       = "kube-system"
     vip_ddns           = false
-    svc_enable         = true
     vip_leaderelection = true
     vip_leaseduration  = 5
     vip_renewdeadline  = 3
