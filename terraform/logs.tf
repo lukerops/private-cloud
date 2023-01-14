700e2e2767a16c01cd0043adadebfc3975da1bea
Lucas Campos Vieira <lucasscvvieira@gmail.com>
2023-01-14 14:44:15 -0300

    adiciona o loki-stack como ferramenta de log

---
+++ terraform/logs.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "loki_stack" {
+  name       = "loki-stack"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "loki-stack"
+  version    = "2.8.9"
+
+  namespace        = "loki-system"
+  create_namespace = true
+  wait             = true
+  wait_for_jobs    = true
+
+  depends_on = [
+    helm_release.kube_prometheus,
+  ]
+}
