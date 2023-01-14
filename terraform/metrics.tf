14f6153fe9cadb26b632308c62ed8fccc533d0b6
Lucas Campos Vieira <lucasscvvieira@gmail.com>
2023-01-14 14:42:40 -0300

    adiciona o metrics-server para obtenção de metricas no k9s

---
+++ terraform/metrics.tf
@@ -0,0 +1,21 @@
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "3.8.2"
+
+  namespace     = "kube-system"
+  wait          = true
+  wait_for_jobs = true
+
+  values = [
+    <<-EOT
+    metrics:
+      enabled: true
+    EOT
+  ]
+
+  depends_on = [
+    helm_release.kube_prometheus,
+  ]
+}
